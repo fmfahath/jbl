2c8df4a890735d8ace844adfe46790ac83a41b96
fmfahath <fmfahath.dev@gmail.com>
2024-01-08 15:34:38 +0530

    header section : hide nav menu when click on each nav link item+

--- app.js
+++ app.js
@@ -15,3 +15,13 @@ if(navClose){
         navMenu.classList.remove('show-menu');
     });
 }
+
+//hide nav menu when we click on nav link item
+const navLink = document.querySelectorAll('.nav__link');
+
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show-menu');
+};
+
+navLink.forEach(n => n.addEventListener('click', linkAction));
