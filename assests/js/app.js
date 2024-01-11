79276809fc19316309e26ebcf2514b31be2b8b7a
fmfahath <fmfahath.dev@gmail.com>
2024-01-11 20:07:57 +0530

    scroll reveal aimation added

--- assests/js/app.js
+++ assests/js/app.js
@@ -87,11 +87,19 @@ const scrollActive = () => {
 
 window.addEventListener('scroll', scrollActive);
 
-const ScrollY = () => {
-    const scrollDown = window.scrollY;
-    console.log(scrollDown);
-    console.log('----------------------------')
-}
 
-// window.addEventListener('scroll', ScrollY);
+// scroll reveal animation-------------------------------------------------------------------------
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    rest: true, //animation repeat
+})
 
+sr.reveal('.home__social, .favorite__container, .sponsor__container, .footer__container');
+sr.reveal('.home__title span:nth-child(1)', {origin: 'left', opacity: 1});
+sr.reveal('.home__title span:nth-child(3)', {origin: 'right', opacity: 1});
+sr.reveal('.home__tooltip, .home__button, .model__button ', {origin: 'bottom'});
+sr.reveal('.about__data', {origin: 'left'});
+sr.reveal('.about__img, .model__tooltip ', {origin: 'right'});
